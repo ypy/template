5f539345f0a740ed654556900f789c800ae65db9
Daniel <yanpingyuan@shinetechchina.com>
2018-11-19 18:05:52 +0800

    aa

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(@Inject(DOCUMENT) private document: any) {
+
+  }
+
   title = 'demo';
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    console.log(this.document.body.scrollTop);
+  }
 }
